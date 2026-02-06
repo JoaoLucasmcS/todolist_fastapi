66036d4e7ccd4bd1d05ae38fdcd9dc929a9d828b
joaomarcol <joaolucasmcs04@gmail.com>
2026-02-06 00:05:38 -0300

    feat: Primeira migration com alembic: criada tabela de usuário

---
+++ tests/test_db.py
@@ -0,0 +1,22 @@
+from sqlalchemy import select
+
+from dataclasses import asdict
+
+from todolist_fastapi.models import User
+
+
+def test_create_user(session):
+    with mock_db_time(model=User) as time:
+        new_user = User(username='alice', password='secret', email='test@test.com')
+        session.add(new_user)
+        session.commit()
+        
+    user = session.scalar(select(User).where(User.username == 'alice'))
+
+    assert asdict(user) == { 
+        'id': 1,
+        'username': 'alice',
+        'password': 'secret',
+        'email': 'teste@test',
+        'created_at': time,  
+    }
